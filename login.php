836bb4b7ae62a34d39e9f5472a13d4597a8b4646
RufusLV <98960144+RufusLV@users.noreply.github.com>
2022-02-18 12:58:15 +0200

    Add files via upload
    logins ar session

--- login.php
+++ login.php
@@ -1,4 +1,29 @@
-<?php include_once 'header.php'; ?>
+<?php 
+include_once 'header.php';
+include_once 'connection.php';
+session_start();
+    $message="";
+    if(count($_POST)>0) {
+        $conn = mysqli_connect('localhost','root','','projekts1') or die('Unable To connect');
+        $result = mysqli_query($conn,"SELECT * FROM users WHERE name='" . $_POST["name"] . "' and pwd = '". $_POST["pwd"]."'");
+        $row  = mysqli_fetch_array($result);
+        if(is_array($row)) {
+        $_SESSION["id"] = $row['id'];
+        $_SESSION["name"] = $row['name'];
+        }
+        if(empty($pwd)){
+            $message = "Ievadiet paroli";
+        }
+        if(empty($name)){
+            $message = "Ievadiet lietotājvārdu";
+        } else {
+         $message = "Nepareiza parole un lietotājvārds";
+        }
+    }
+    if(isset($_SESSION["id"])) {
+    header("Location:index.php");
+    }
+?>
 
     <section class="signup-form">
 
@@ -6,6 +31,7 @@
             <h2>Ielogošanās</h2>
             <form action="" method="post">
                 <div class="input-group">
+                <?php if($message!="") { echo $message; } ?>
                     <input type="text" name="name" placeholder="Lietotājvārds / Epasts">
                 </div>
 
